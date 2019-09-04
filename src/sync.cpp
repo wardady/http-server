afd9eec46b712921253d469133bd56f823de139c
Hermann <yavorskyi@ucu.edu.ua>
2019-09-05 01:37:28 +0300

    Sync implementation
     Auto stash before merge of "master" and "origin/master"

--- src/sync.cpp
+++ src/sync.cpp
@@ -1,20 +1,14 @@
-// This is a personal academic project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-
+#include <cli.h>
+#include <boost/asio/io_service.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <thread>
+#include <iostream>
+#include <casher.h>
 #include <boost/asio/streambuf.hpp>
+#include <http.h>
+#include <boost/asio/completion_condition.hpp>
 #include <boost/asio/read.hpp>
 #include <boost/asio/write.hpp>
-#include <boost/asio/io_service.hpp>
-#include "cli.h"
-
-#include <iostream>
-#include <string>
-#include <thread>
-#include <vector>
-
-#include "http.h"
-#include "casher.h"
 
 namespace asio = boost::asio;
 
@@ -32,19 +26,18 @@ public:
 
     // read client's http request
     void on_connect() {
-        asio::async_read(socket, read_buff, asio::transfer_at_least(1),
-                         std::bind(&session::on_read, shared_from_this(), std::placeholders::_1,
-                                   std::placeholders::_2));
+        boost::system::error_code ec;
+        std::size_t read = asio::read(socket, read_buff, asio::transfer_at_least(1),ec);
+        on_read(ec, read);
     }
 
     // callback for reading client's http request
-    void on_read(boost::system::error_code ec, std::size_t s) {
+    void on_read(boost::system::error_code &ec, std::size_t s) {
         if (!ec) {
             http::request req(read_buff);
             req.write_response(write_buff, doc_root, std::ref(cash));
-            asio::async_write(socket, write_buff,
-                              std::bind(&session::on_write, shared_from_this(), std::placeholders::_1,
-                                        std::placeholders::_2));
+            std::size_t wrote = asio::write(socket, write_buff, asio::transfer_all(), ec);
+            on_write(ec, wrote);
         } else
             std::cerr << "Reading request error: " << ec.message() << std::endl;
     }
@@ -59,16 +52,17 @@ public:
     }
 };
 
-class listener : public std::enable_shared_from_this<listener> {
+
+class listener {
     asio::ip::tcp::acceptor acceptor_;
-    asio::ip::tcp::socket socket_;
     const std::string &doc_root_;
+    asio::io_context &ioc_;
     casher cash;
 
 public:
     listener(asio::io_context &ioc, const asio::ip::tcp::endpoint &endpoint, const std::string &doc_root,
              const bool no_cache)
-            : acceptor_(ioc), socket_(ioc), doc_root_(doc_root), cash(!no_cache) {
+            : acceptor_(ioc), doc_root_(doc_root), cash(!no_cache), ioc_(ioc) {
         boost::system::error_code ec;
 
         // initialize acceptor by endpoint
@@ -88,46 +82,37 @@ public:
             return;
         std::cout << "Running on http://" << acceptor_.local_endpoint().address() << ":"
                   << acceptor_.local_endpoint().port() << "/ (Press Ctrl+C to quit)" << std::endl;
-        do_accept();
+        listen();
     }
+
 
     // accept connections
-    void do_accept() {
-        acceptor_.async_accept(socket_, std::bind(&listener::on_accept, shared_from_this(), std::placeholders::_1));
+    [[noreturn]]void listen() {
+        for (;;) {
+            asio::ip::tcp::socket socket{ioc_};
+            acceptor_.accept(socket);
+            // initialize session
+            auto sesh = std::make_shared<session>(std::move(socket), doc_root_, std::ref(cash));
+            std::thread(&session::run, sesh).detach();
+        }
     }
 
-    // callback for establishing connection
-    void on_accept(boost::system::error_code ec) {
-        if (!ec)
-            // initialize session
-            std::make_shared<session>(std::move(socket_), doc_root_, std::ref(cash))->run();
-        else
-            std::cerr << "Accepting connection error: " << ec.message() << std::endl;
-        // accept next connection
-        do_accept();
-    }
+
 };
 
 int main(int argc, char *argv[]) {
     try {
         cli cli_ob{argc, argv};
         // create ioservice shared between all threads
-        asio::io_service ioc{cli_ob.get_threads()};
+        asio::io_service ioc{1};
         // initialize and run listener
-        std::make_shared<listener>(ioc, asio::ip::tcp::endpoint{asio::ip::tcp::v4(),
-                                                                cli_ob.get_port()},
-                                   cli_ob.get_directory(), cli_ob.get_no_cache())->run();
-
-        // run ioservice in every thread
-        std::vector<std::thread> thread_vector;
-        thread_vector.reserve(cli_ob.get_threads() - 1);
-        for (auto i = cli_ob.get_threads() - 1; i > 0; --i)
-            thread_vector.emplace_back([&ioc] { ioc.run(); });
-        ioc.run();
+        listener listener_{ioc, asio::ip::tcp::endpoint{asio::ip::tcp::v4(),
+                                                        cli_ob.get_port()}, cli_ob.get_directory(),
+                           cli_ob.get_no_cache()};
+        listener_.run();
 
         return 0;
-    }
-    catch (const std::exception &err) {
+    } catch (const std::exception &err) {
         err.what();
         return 1;
     }
